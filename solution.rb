58166d4b77e053e109d3b5416ca4871168509724
Reyes2777 <jonathan.reyes2777@gmail.com>
2018-10-25 18:51:13 -0500

    fix solution

--- solution.rb
+++ solution.rb
@@ -3,6 +3,8 @@ require "sinatra"
 get '/' do
     if params == {}
         @name = "desconocido"
+    elsif params == {"nombre" => ""}
+        @name = "desconocido"
     else
     @name = params["nombre"]
     end
